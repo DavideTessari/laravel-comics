8be72ac785d3ca9a8122bf3a230f52cadafde0c7
DavideTessari <davtex@gmail.com>
2024-05-20 18:55:24 +0200

    add jumbotropn section

---
+++ resources/views/partials/jumbotron.blade.php
@@ -0,0 +1,12 @@
+@php
+    $header_link = config('menus-db.headerLink')
+@endphp
+<div class="main-wrapper jumbotron">
+
+    <div class="container">
+        <div class="current-series">
+            <span>{{ Route::currentRouteName()}}</span>
+        </div>
+    </div>
+
+</div>
